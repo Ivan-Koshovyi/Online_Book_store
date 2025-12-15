adcd01293f8d03f57d7f5e5f394bcb537ed0c2f1
ivankosh <ivan2023koshovyi@gmail.com>
2025-12-15 19:16:44 +0200

    Replaced the existing BookRepository with the one that will use JpaRepository interface

--- src/main/java/store/repository/BookRepository.java
+++ src/main/java/store/repository/BookRepository.java
@@ -1,13 +1,6 @@
 package store.repository;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import store.model.Book;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
-}
+public interface BookRepository extends JpaRepository<Book, Long> {}
