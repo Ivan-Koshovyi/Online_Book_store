f1161fc5c11a2d291f920fb85d4c4c997e54bffc
ivankosh <ivan2023koshovyi@gmail.com>
2026-01-17 21:12:44 +0200

    check style

--- src/main/java/store/dto/CreateBookRequestDto.java
+++ src/main/java/store/dto/CreateBookRequestDto.java
@@ -1,7 +1,8 @@
 package store.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,17 +10,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
     private BigDecimal price;
-    @NotNull
+    @NotBlank
     private String description;
-    @NotNull
+    @NotBlank
     private String coverImage;
 }
--- src/main/java/store/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/store/exception/CustomGlobalExceptionHandler.java
@@ -25,14 +25,17 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
             HttpHeaders headers,
             HttpStatusCode status,
             WebRequest request) {
-        Map<String,Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST);
-        List<String> errors = ex.getBindingResult().getAllErrors().stream()
-                .map(this::getErrorMessage)
-                .toList();
-        body.put("errors", errors);
-        return new ResponseEntity<>(body, headers, status);
+        try {
+            Map<String,Object> body = new LinkedHashMap<>();
+            body.put("timestamp", LocalDateTime.now());
+            List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                    .map(this::getErrorMessage)
+                    .toList();
+            body.put("errors", errors);
+            return new ResponseEntity<>(body, headers, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Can't process validation errors");
+        }
     }
 
     private String getErrorMessage(ObjectError e) {
