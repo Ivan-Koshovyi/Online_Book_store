58d86f2701016840f4cac6c8229db16304a217b5
ivankosh <ivan2023koshovyi@gmail.com>
2026-02-19 18:31:45 +0200

    Implemented UserDetails interface in the User class

--- src/main/java/store/repository/UserRepository.java
+++ src/main/java/store/repository/UserRepository.java
@@ -2,7 +2,10 @@ package store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.model.User;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
