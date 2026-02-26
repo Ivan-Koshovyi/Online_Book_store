23c4fec5c27d6229b2e0de65e6718efb0b13af97
ivankosh <ivan2023koshovyi@gmail.com>
2026-02-26 20:12:58 +0200

    Task is completed

--- src/main/java/store/controller/AuthenticationController.java
+++ src/main/java/store/controller/AuthenticationController.java
@@ -2,7 +2,6 @@ package store.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +10,26 @@ import store.dto.UserLoginRequestDto;
 import store.dto.UserLoginResponseDto;
 import store.dto.UserRegistrationRequestDto;
 import store.dto.UserResponseDto;
+import store.service.AuthenticationService;
 import store.service.UserService;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto registerUser(@Valid @RequestBody UserRegistrationRequestDto user) {
         return userService.registerUser(user);
     }
 
-    @GetMapping("/login")
-    public UserLoginResponseDto login(UserLoginRequestDto request) {
-        return null;
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto request) {
+        System.out.println("Email received: " + request.getEmail());
+        System.out.println("Password received: " + request.getPassword());
+        return authenticationService.authenticate(request);
     }
 }
