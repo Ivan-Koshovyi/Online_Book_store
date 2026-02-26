27da0385e453d7fec9964f83f78a5e2f77c39d81
ivankosh <ivan2023koshovyi@gmail.com>
2026-02-26 18:27:24 +0200

    Added the following method to the AuthenticationController, and implemented some dto

--- src/main/java/store/controller/AuthenticationController.java
+++ src/main/java/store/controller/AuthenticationController.java
@@ -2,10 +2,13 @@ package store.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.dto.UserLoginRequestDto;
+import store.dto.UserLoginResponseDto;
 import store.dto.UserRegistrationRequestDto;
 import store.dto.UserResponseDto;
 import store.service.UserService;
@@ -19,5 +22,10 @@ public class AuthenticationController {
     @PostMapping("/registration")
     public UserResponseDto registerUser(@Valid @RequestBody UserRegistrationRequestDto user) {
         return userService.registerUser(user);
+    }
+
+    @GetMapping("/login")
+    public UserLoginResponseDto login(UserLoginRequestDto request) {
+        return null;
     }
 }
---
+++ src/main/java/store/dto/UserLoginRequestDto.java
@@ -0,0 +1,8 @@
+package store.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequestDto(@NotBlank @Size(min = 8, max = 20) String email,
+                                  @NotBlank @Size(min = 8, max = 20) String password) {
+}
