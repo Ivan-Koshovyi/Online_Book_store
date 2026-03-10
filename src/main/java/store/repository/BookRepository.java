2f4bcba133b1ce05e293c5c8d86a657aea7700e0
ivankosh <ivan2023koshovyi@gmail.com>
2026-03-10 22:10:53 +0200

    check style

--- src/main/java/store/repository/BookRepository.java
+++ src/main/java/store/repository/BookRepository.java
@@ -1,11 +1,10 @@
 package store.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import store.model.Book;
 
-import java.util.List;
-
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-    List<Book> findAllByCategoryId(Long categoryId);
+    List<Book> findAllByCategoriesId(Long categoryId);
 }
