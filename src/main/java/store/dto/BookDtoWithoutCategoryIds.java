8322c337c17d7b8bd7176bd9393064c3a5a51ffb
ivankosh <ivan2023koshovyi@gmail.com>
2026-03-10 22:18:28 +0200

    Add BookDtoWithoutCategoryIds

---
+++ src/main/java/store/dto/BookDtoWithoutCategoryIds.java
@@ -0,0 +1,17 @@
+package store.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class BookDtoWithoutCategoryIds {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
