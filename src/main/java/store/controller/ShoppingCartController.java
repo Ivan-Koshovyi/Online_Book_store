e450eb4f4932e55b5dc0908f0ac3bf176919a411
ivankosh <ivan2023koshovyi@gmail.com>
2026-04-01 21:01:21 +0300

    Add swagger

--- src/main/java/store/controller/ShoppingCartController.java
+++ src/main/java/store/controller/ShoppingCartController.java
@@ -1,8 +1,10 @@
 package store.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,22 +28,30 @@ public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
 
+    @Operation(summary = "Get shopping cart")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     private ShoppingCartDto getShoppingCart(@AuthenticationPrincipal UserDetails userDetails) {
         return shoppingCartService.getCartByUserId(userDetails.getUsername());
     }
 
+    @Operation(summary = "Put item in shopping cart")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PostMapping
     private CartItemDto postItemInCart(@RequestBody @Valid BookInCartDto book,
                                        @AuthenticationPrincipal UserDetails userDetails) {
         return shoppingCartService.postItem(book, userDetails.getUsername());
     }
 
+    @Operation(summary = "Update item in shopping cart")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PutMapping("/items/{Id}")
     private CartItemDto updateItemInCart(@PathVariable Long id, int quantity) {
         return shoppingCartService.updateItem(id,quantity);
     }
 
+    @Operation(summary = "Delete item in shopping cart")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @DeleteMapping("/items/{Id}")
     private void deleteItemInCart(@PathVariable Long id) {
         shoppingCartService.deleteItem(id);
